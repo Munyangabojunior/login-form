09476a073c80f5651eb3190e4d29ea738643d4d9
Munyangabo junior <Munyangabo4@gmail.com>
2024-05-29 10:56:30 +0200

    general commit

---
+++ assets/js/main.js
@@ -0,0 +1,22 @@
+/*=============== SHOW & HIDE PASSWORD ===============*/
+const input = document.getElementById("login-pass"),
+    eyeIcon = document.getElementById("login-eye");
+
+eyeIcon.addEventListener("click", ()=>{
+    // Change input type from password to text
+    if(input.type == 'password'){
+        // Set input type as text
+        input.type="text";
+
+        // change icon
+        eyeIcon.classList.add("fa-eye");
+        eyeIcon.classList.remove("fa-eye-slash");
+    }else{
+        // Change to password
+        input.type = "password";
+
+        // Change icon
+        eyeIcon.classList.remove("fa-eye");
+        eyeIcon.classList.add("fa-eye-slash");
+    }
+})
